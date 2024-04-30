098be3cf85cb9a1015617b9a595b0c3646bcaaf5
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-04-30 16:06:49 +0900

    ECSのログ出力を設定

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -2,6 +2,15 @@ resource "aws_ecs_cluster" "eval_sleuth" {
   name = "eval_sleuth"
 }
 
+# resource "aws_ecs_task_definition" "eval_sleuth" {
+#   family                   = "eval_sleuth"
+#   cpu                      = "256"
+#   memory                   = "512"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   container_definitions    = file("./container_definitions.json")
+# }
+
 resource "aws_ecs_task_definition" "eval_sleuth" {
   family                   = "eval_sleuth"
   cpu                      = "256"
@@ -9,6 +18,7 @@ resource "aws_ecs_task_definition" "eval_sleuth" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
 }
 
 resource "aws_ecs_service" "eval_sleuth" {
@@ -47,4 +57,31 @@ module "nginx_sg" {
   vpc_id      = aws_vpc.eval_sleuth.id
   port        = 80
   cidr_blocks = [aws_vpc.eval_sleuth.cidr_block]
+}
+
+resource "aws_cloudwatch_log_group" "for_ecs" {
+  name              = "/ecs/eval_sleuth"
+  retention_in_days = 180
+}
+
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_policy_documents = [data.aws_iam_policy.ecs_task_execution_role_policy.policy]
+  //source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "./module/iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
